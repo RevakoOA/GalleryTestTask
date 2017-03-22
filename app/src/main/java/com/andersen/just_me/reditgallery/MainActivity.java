31a002babbe11ca250ab93ed15738cb9338e9bdc
Ostap <revakoOA@gmail.com>
2017-03-22 21:15:40 +0200

    Empty project. git init.

---
+++ app/src/main/java/com/andersen/just_me/reditgallery/MainActivity.java
@@ -0,0 +1,16 @@
+package com.andersen.just_me.reditgallery;
+
+import android.os.Bundle;
+import android.support.v4.app.SupportActivity;
+
+/**
+ * Created by just_me on 22.03.17.
+ */
+
+public class MainActivity extends SupportActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
